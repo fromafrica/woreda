824b79c36991ffef594ccbcc745ea799f0f9c0e7
Sami Fouad <sfouad@gmail.com>
2023-10-02 22:29:42 -0600

    switch to react

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
 export { Button } from "../stories/Button";
+export type { ButtonProps } from "../stories/Button";
+export { Header } from "../stories/Header";
+export type { HeaderProps } from "../stories/Header";
+export type { User } from "../stories/Header";
